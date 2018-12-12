9d946006bdab666c8d9dc6fe434ddfde8d934061
ydh1304 <donghyun.lee@mvlchain.io>
2018-12-12 17:15:23 +0900

    date extension

--- Sources/Extensions/SirenDateExtension.swift
+++ Sources/Extensions/SirenDateExtension.swift
@@ -28,4 +28,22 @@ extension Date {
 
         return days(since: releaseDate)
     }
+
+    static func hours(since date: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour], from: date, to: Date())
+        return components.hour ?? 0
+    }
+    
+    static func hours(since dateString: String) -> Int? {
+        let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "en_US_POSIX")
+        dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        
+        guard let releaseDate = dateformatter.date(from: dateString) else {
+            return nil
+        }
+        
+        return hours(since: releaseDate)
+    }
 }
